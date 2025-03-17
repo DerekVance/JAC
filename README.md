64ff5817f3753852382d680446f6c78ecb9d0b5e
Derek Vance <derek.vance.work@gmail.com>
2025-03-16 23:57:25 -0400

    First commit blank state

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+This is an AI chatbot built with a theme library using react native.
---
+++ src/screens/Home.tsx
@@ -0,0 +1,28 @@
+import React, {useCallback, useState} from 'react';
+
+import {useData, useTheme, useTranslation} from '../hooks/';
+import {Block, Button, Image, Input, Product, Text} from '../components/';
+
+const Home = () => {
+  const {t} = useTranslation();
+  const [tab, setTab] = useState<number>(0);
+  const {following, trending} = useData();
+  const [products, setProducts] = useState(following);
+  const {assets, colors, fonts, gradients, sizes} = useTheme();
+
+  const handleProducts = useCallback(
+    (tab: number) => {
+      setTab(tab);
+      setProducts(tab === 0 ? following : trending);
+    },
+    [following, trending, setTab, setProducts],
+  );
+
+  return (
+    <Block>
+    
+    </Block>
+  );
+};
+
+export default Home;
